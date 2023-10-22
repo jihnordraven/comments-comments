790d7ca25236088721139f7461209c99d0bb4cc9
flyingMerch123 <=>
2023-10-22 18:13:02 +0300

    fix: fix alias

--- src/guards-handlers/strategies/jwt.strategy.ts
+++ src/guards-handlers/strategies/jwt.strategy.ts
@@ -1,8 +1,8 @@
-import { CONFIG } from '@config'
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { JwtAccessPayload } from '../guards/jwt-ws.guard'
+import { CONFIG } from '../../config'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
