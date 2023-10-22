1be94183abd8c9e7de0ca158914086a4e06ff329
flyingMerch123 <=>
2023-10-22 19:51:00 +0300

    hot-fix: types-error

--- src/utils/decorators/jwt-access-payload.decorator.ts
+++ src/utils/decorators/jwt-access-payload.decorator.ts
@@ -4,7 +4,7 @@ import { JwtAccessPayload } from '../../guards-handlers/guards'
 
 export const JwtAccessPayloadDecorator = createParamDecorator(
 	(key: keyof JwtAccessPayload, ctx: ExecutionContext): any => {
-		const req: Request = ctx.switchToHttp().getRequest()
+		const req: Request & { user: JwtAccessPayload } = ctx.switchToHttp().getRequest()
 
 		return key ? req.user[key] : req.user
 	}
