c6958495fe568b8b11ed5be3b04a20fdfe4239d2
flyingMerch123 <=>
2023-10-24 14:29:44 +0300

    fix: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { CommentsModule } from './modules/comments/comments.module'
-import { PrismaModule } from 'prisma/prisma.module'
+import { PrismaModule } from '../prisma/prisma.module'
 import { STRATEGIES } from './guards-handlers/strategies'
 import { JwtWsGuard } from './guards-handlers/guards'
 import { AppController } from './app.controller'
